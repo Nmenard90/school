e85237e83e50f3228fafd7dc97bc8b074cf1a506
Nmenard90 <111668934+Nmenard90@users.noreply.github.com>
2022-08-24 13:18:40 -0500

    perimeter of a circle
    #perimiter = 3.14159 * radiusofmycircle * 2.0 - forumla needed
    whatwastyped = input("What is the radius of the circle?") -  saving what is typed
    radiusofmycircle = float(whatwastyped)  - converting numbers into decimal places
    perimiter = 3.14159 * radiusofmycircle * 2.0  - defining formula 
    print("The perimiter of the circle with a radius of", radiusofmycircle, "inches has a perimiter of", perimiter, "square inches") - printing answer based on the parameters set in place

---
+++ 8-24-2022.py
@@ -0,0 +1,6 @@
+#perimiter = 3.14159 * radiusofmycircle * 2.0
+whatwastyped = input("What is the radius of the circle?")
+radiusofmycircle = float(whatwastyped)
+perimiter = 3.14159 * radiusofmycircle * 2.0
+print("The perimiter of the circle with a radius of", radiusofmycircle, "inches has a perimiter of", perimiter, "square inches")
+
